db20fdfc3efe58746fa49fcfb47d1eea5ed53890
tjrqh <asdww6631@gmail.com>
2025-06-26 19:11:46 +0900

    #24 feat : get user key value by token parsing

---
+++ src/main/java/ticketing/ticketing/infrastructure/security/UserContext.java
@@ -0,0 +1,18 @@
+package ticketing.ticketing.infrastructure.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContext {
+
+    public  Long getCurrentUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof Long userId) {
+            return userId;
+        }
+        return null;
+    }
+}
