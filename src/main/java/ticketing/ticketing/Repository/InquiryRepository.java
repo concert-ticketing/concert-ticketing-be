4aeaba6e0c4bec07a4711641bd4b89508afe1234
tjrqh <asdww6631@gmail.com>
2025-08-04 01:44:43 +0900

    #33 fix : merge modify

--- src/main/java/ticketing/ticketing/Repository/InquiryRepository.java
+++ src/main/java/ticketing/ticketing/Repository/InquiryRepository.java
@@ -4,8 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ticketing.ticketing.DTO.InquiryResponseDto;
 import ticketing.ticketing.domain.entity.Inquiry;
+import ticketing.ticketing.domain.enums.InquiryStatus;
 
 import java.util.Optional;
 
@@ -19,4 +23,23 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     @NotNull
     Page<Inquiry> findAll(Pageable pageable);
 
+    @Query("""
+  SELECT new ticketing.ticketing.DTO.InquiryResponseDto(
+    i.id,
+    i.user.id,
+    i.user.name,
+    i.user.email,
+    i.title,
+    i.content,
+    i.type,
+    i.status,
+    i.repliedAt,
+    i.createdAt,
+    i.updatedAt,
+    i.deletedAt
+  )
+  FROM Inquiry i
+  WHERE i.status = :status
+""")
+    Page<InquiryResponseDto> findAllByStatus(@Param("status") String status, Pageable pageable);
 }
